52b130e7ac88b2b924eede598337e0fe3350752a
carletonDev <carletonDev@users.noreply.github.com>
2019-11-12 09:34:24 -0600

    needed to rename jooq auto gen small bug where the auto gen package can't be found by the compiler

--- src/main/java/com/example/application/spring/Application.java
+++ src/main/java/com/example/application/spring/Application.java
@@ -14,29 +14,29 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
-//        Configuration configuration = new Configuration()
-////                .withJdbc(new Jdbc()
-////                        .withDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver")
-////                        .withUrl("jdbc:sqlserver://carletonserver.database.windows.net:1433;database=Pharmacy;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;")
-////                        .withUsername("carl")
-////                        .withPassword("5611S@ddle")
-////                        .withUser("carl@carletonserver")
-////                        .withAutoCommit(true))
-////                .withGenerator(new Generator()
-////                        .withDatabase(new Database().withIncludes("Pharmacy|Medicine|PharmacyMedicine|Roles|Salary|Transactions|Users"))
-////                        .withGenerate(new Generate()
-////                                .withRelations(true)
-////                                .withPojos(true)
-////                                .withJpaAnnotations(true)
-////                                .withInterfaces(true)
-////                                .withDaos(true)
-////                                .withSpringAnnotations(true)
-////                                .withDeprecated(false)
-////                        )
-////                        .withTarget(new Target()
-////                                .withPackageName("Pharmacy")
-////                                .withDirectory("src/main/java")));
-////       GenerationTool.generate(configuration);
+        Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc()
+                        .withDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                        .withUrl("jdbc:sqlserver://carletonserver.database.windows.net:1433;database=Pharmacy;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;")
+                        .withUsername("carl")
+                        .withPassword("5611S@ddle")
+                        .withUser("carl@carletonserver")
+                        .withAutoCommit(true))
+                .withGenerator(new Generator()
+                        .withDatabase(new Database().withIncludes("Pharmacy|Medicine|PharmacyMedicine|Roles|Salary|Transactions|Users"))
+                        .withGenerate(new Generate()
+                                .withRelations(true)
+                                .withPojos(true)
+                                .withJpaAnnotations(true)
+                                .withInterfaces(true)
+                                .withDaos(true)
+                                .withSpringAnnotations(true)
+                                .withDeprecated(false)
+                        )
+                        .withTarget(new Target()
+                                .withPackageName("Pharmacy")
+                                .withDirectory("src/main/java")));
+       GenerationTool.generate(configuration);
         SpringApplication.run(Application.class, args);
 
 
